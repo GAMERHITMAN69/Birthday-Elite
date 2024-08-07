871fb4ed18d8eaa9d32a181386dcc4da10a42678
GAMER_HITMAN <gamerhitman69@gmail.com>
2024-08-07 08:10:28 +0500

    Final Commit

--- js/index.js
+++ js/index.js
@@ -294,7 +294,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "https://www.youtube.com/embed/K2aJTT29ZdU?controls=0&loop=1&autoplay=1");
+  ifrm.setAttribute("src", "https://www.youtube.com/embed/n3qw7lZzbkM?autoplay=1&controls=0&loop=1");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
